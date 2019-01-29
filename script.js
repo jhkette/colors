dd02bb65f482d273601f846898ade87cadc51f2c
khenryjoe <joseph.ketterer@gmail.com>
2019-01-29 00:54:34 +0000

    tiyding

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-window.onload = function(){
-/*use display mtaches from javascript lessons  */
+window.onload = function() {
+    /*use display mtaches from javascript lessons  */
 
-
+    //
     const cities = [];
 
     fetch("./data.JSON")
@@ -10,47 +10,41 @@ window.onload = function(){
 
 
 
-// function processData(data) {
-//     console.log(data);
-// }
+    function findMatches(wordToMatch, cities) {
+        return cities.filter(place => {
+            // here we need to figure out if the city or state matches what was searched
+            const regex = new RegExp(wordToMatch, 'gi');
+            return place.color.match(regex)
+        });
+    }
 
-// use container width to generate colours
-// var screenWidth = container.offsetWidth;  // get container width
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
 
-function findMatches(wordToMatch, cities) {
-  return cities.filter(place => {
-    // here we need to figure out if the city or state matches what was searched
-    const regex = new RegExp(wordToMatch, 'gi');
-    return place.color.match(regex)
-  });
-}
+    function displayMatches() {
+        const matchArray = findMatches(this.value, cities);
+        const html = matchArray.map(place => {
+            const regex = new RegExp(this.value, 'gi');
+            const cityName = place.color.replace(regex, `<span class="hl">${this.value}</span>`);
+            const styles = place.hex;
 
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
+            console.log(styles);
 
-function displayMatches() {
-  const matchArray = findMatches(this.value, cities);
-  const html = matchArray.map(place => {
-    const regex = new RegExp(this.value, 'gi');
-    const cityName = place.color.replace(regex, `<span class="hl">${this.value}</span>`);
-    const styles = place.hex;
-    console.log(styles);
-
-    return `
+            return `
       <li>
-        <span class="name" style= "color:${styles}">${cityName},</span>
+        <span class="name" style= "color:${styles}">${cityName}: ${styles},</span>
 
       </li>
     `;
-  }).join('');
-  suggestions.innerHTML = html;
-}
+        }).join('');
+        suggestions.innerHTML = html;
+    }
 
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
+    const searchInput = document.querySelector('.search');
+    const suggestions = document.querySelector('.suggestions');
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+    searchInput.addEventListener('change', displayMatches);
+    searchInput.addEventListener('keyup', displayMatches);
 
 }
